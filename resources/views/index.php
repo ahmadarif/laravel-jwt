60dd75272a3cf175144a1f9aa3a4ba3e8d1d17f1
Ahmad Arif <ahmad.arif019@gmail.com>
2017-04-04 20:29:21 +0700

    update konten index.php

--- resources/views/index.php
+++ resources/views/index.php
@@ -2,24 +2,9 @@
 <html>
 <head>
     <meta charset="utf-8">
-    <title>Angular-Laravel Authentication</title>
-    <link rel="stylesheet" href="node_modules/bootstrap/dist/css/bootstrap.css">
+    <title>Laravel + JWT + Dingo API</title>
 </head>
-<body ng-app="authApp">
-
-<div class="container">
-    <div ui-view></div>
-</div>
-
+<body>
+    <h1>Selamat datang!</h1>
 </body>
-
-<!-- Application Dependencies -->
-<script src="node_modules/angular/angular.js"></script>
-<script src="node_modules/angular-ui-router/build/angular-ui-router.js"></script>
-<script src="node_modules/satellizer/satellizer.js"></script>
-
-<!-- Application Scripts -->
-<script src="scripts/app.js"></script>
-<script src="scripts/authController.js"></script>
-<script src="scripts/userController.js"></script>
 </html>
